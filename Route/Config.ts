54c226eadf30ae4c57ba1a0113d484159e68abaf
Sora210 <sokominto1231@gmail.com>
2022-05-16 05:17:00 +0000

    [update] config取得仕組み変更

--- Route/Config.ts
+++ Route/Config.ts
@@ -1,19 +1,14 @@
-//import & instance準備
+//import & instance
 import { Router } from 'express'
 import path = require("path");
 import fs = require("fs");
-const ConfigRouter = Router()
+const ConfigRouter = Router();
 
-ConfigRouter.get('/config/inference/:cameraId', (req, res) => {
-    const dirPath = path.join('/work/config');
-    const list = fs.readdirSync(dirPath, {withFileTypes: true})
-          .filter(dirent => dirent.isFile()).map(({name}) => name)
-          .filter(function(file) 
-          {
-              return file === 'inference.json';
-          });
-    const json = JSON.parse(fs.readFileSync(path.join(dirPath, "/" + list[0]), "utf8"))
-    res.status(200).json(json)
+const rootDirPath = path.join('/work/config');
+
+ConfigRouter.get('/config/:cameraId/inference', (req, res) => {
+    const json = JSON.parse(fs.readFileSync(path.join(rootDirPath, req.params.cameraId,"inference.json"), "utf8"));
+    res.status(200).json(json);
 })
 
 ConfigRouter.all('*', (req, res, next) => {
