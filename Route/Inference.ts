fe4302c6c556d36571ec1c475797260deb845ce6
Sora210 <sokominto1231@gmail.com>
2022-05-16 05:17:54 +0000

    [update] 推論結果取得リンク変更

--- Route/Inference.ts
+++ Route/Inference.ts
@@ -1,20 +1,32 @@
-//import & instance準備
+//import & instance
 import { Router } from 'express'
 import path = require("path");
 import fs = require("fs");
+import glob = require("glob");
 const InferenceRouter = Router()
 
-//最新推論結果取得
-InferenceRouter.get('/inference/:cameraId', (req, res) => {
-    const dirPath = path.join('/work/inference');
-    const list = fs.readdirSync(dirPath, {withFileTypes: true})
-          .filter(dirent => dirent.isFile()).map(({name}) => name)
-          .filter(function(file) 
-          {
-              return path.extname(file).toLowerCase() === '.json';
-          });
-    const json = JSON.parse(fs.readFileSync(path.join(dirPath, "/" + list[list.length - 1]), "utf8"))
-    res.status(200).json(json)
+//最新推論結果
+InferenceRouter.get('/:cameraId', (req, res) => {
+    const dirPath = path.join('/work/Inference/testProject', req.params.cameraId);
+    let fileName:string = "";
+    glob('**/**/*.json', {cwd:dirPath} , (e, files) => {
+        fileName = files[files.length - 1];
+        
+        const json = JSON.parse(fs.readFileSync(path.join(dirPath, fileName), "utf8"));
+        res.status(200).json(json);
+    })
+})
+
+InferenceRouter.get('/:cameraId/:year/:month/:fileName', (req, res) => {
+    const dirPath = path.join('/work/Inference/testProject', req.params.cameraId, req.params.year, req.params.month);
+    let fileName:string = req.params.fileName + ".json";
+
+    try {
+        const json = JSON.parse(fs.readFileSync(path.join(dirPath, fileName), "utf8"));
+        res.status(200).json(json);
+    } catch {
+        res.status(404).json("NotFound")
+    }
 })
 
 InferenceRouter.all('*', (req, res, next) => {
